d47f84f7803a8006d17cb45bd69c0924cf51da98
Krutika <krutikapowle21@gmail.com>
2026-02-28 22:56:06 +0530

    Teacher UI setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import TeacherHome from "./Components/teacherui/TeacherHome";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <TeacherHome />
+    </BrowserRouter>
+  );
+}
+
+export default App;
