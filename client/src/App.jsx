643ab7049c4b059ffb99f444eb270124dd846d3c
Krutika <krutikapowle21@gmail.com>
2026-02-28 23:13:17 +0530

    Resolve staged changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import TeacherHome from "./Components/teacherui/TeacherHome";
 
@@ -10,3 +11,21 @@ function App() {
 }
 
 export default App;
+=======
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+
+    </>
+  )
+}
+
+export default App
+>>>>>>> ce382fd973cf85afc7c3a52135d944e732741fd5
